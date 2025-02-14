e545a81102236912847b96d87c179ad45754389c
Azar <azaralampanah2@gmail.com>
2025-02-14 11:35:05 +0000

    parsequery

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+// function parseQueryString(queryString) {
+//     const queryParams = {};
+//     console.log(queryParams)
+//     const keyValuePair = queryString.split("=");
+//     const key = keyValuePair[0]; // key will hold 'fruit'
+//     const value = keyValuePair[1]; // value will hold 'banana'
+//     queryParams.key = value;
+//   console.log(queryParams)
+//     return queryParams;
+//   }
 function parseQueryString(queryString) {
-    const queryParams = {};
-    console.log(queryParams)
-    const keyValuePair = queryString.split("=");
-    const key = keyValuePair[0]; // key will hold 'fruit'
-    const value = keyValuePair[1]; // value will hold 'banana'
-    queryParams.key = value;
-  console.log(queryParams)
+
+    const queryParams = {}; 
+    const keyValuePairs = queryString.split("&"); 
+    for (const pair of keyValuePairs) {
+      const [key, value] = pair.split("=");
+      queryParams[key] = value;
+    }
+  
     return queryParams;
   }
+  
+  parseQueryString("sort=lowest&colour=yellow");
